38c8605e8a145d0dc4bbeaf4567ebef1ff7d0cc4
citeFred <citefred@gmail.com>
2025-06-26 21:14:38 +0900

    chore: Lombok을 통한 Setter 생성

--- src/main/java/com/metaverse/springpreparedemo/Memo.java
+++ src/main/java/com/metaverse/springpreparedemo/Memo.java
@@ -1,8 +1,10 @@
 package com.metaverse.springpreparedemo;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class Memo {
     private String username;
     private String contents;
